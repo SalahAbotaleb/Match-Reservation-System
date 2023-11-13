dc1e993664183e8efcc35b7337a4a0bf72cfec3f
salah <salah.m.abotaleb@gmail.com>
2023-11-13 20:50:39 +0200

    adding user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    }, password: {
+        type: String,
+        required: true
+    }, firstName: {
+        type: String,
+        required: true
+    }, lastName: {
+        type: String,
+        required: true
+    }, birthDate: {
+        type: Date,
+        required: true
+    }, gender: {
+        type: String,
+        required: true
+    }, city: {
+        type: String,
+        required: true
+    }, address: {
+        type: String,
+    }, email: {
+        type: String,
+        required: true
+    }, role: {
+        type: string
+    }, pending: {
+        type: Boolean
+    }, tickets: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Ticket'
+    }
+
+});
+
+module.exports = mongoose.model("User", userSchema);
