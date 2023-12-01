7549a78c532994f70a9eee45b1ac6428ddc4c271
salah <salah.m.abotaleb@gmail.com>
2023-12-01 17:55:33 +0200

    adding user and matches routes

--- backend/models/adminModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const adminSchema = new Schema({});
-module.exports = mongoose.model("Admin", adminSchema);
--- backend/utils/authorizeUser.js
+++ backend/utils/authorizeUser.js
@@ -3,6 +3,12 @@ module.exports = (userType) => {
         if (req.session.user_id == null) {
             return res.status(401).send("Unauthorized");
         }
+        if (req.session.status == "pending") {
+            return res.status(200).send("Your account is still pending");
+        }
+        if (req.session.status == "rejected") {
+            return res.status(200).send("Your account request is rejected");
+        }
         if (req.session.user_role == userType) {
             return next();
         }
