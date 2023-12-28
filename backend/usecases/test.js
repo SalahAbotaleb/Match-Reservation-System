e40d2040c5c0492bb674233d2ea3215e6b87f891
salah <salah.m.abotaleb@gmail.com>
2023-12-29 01:12:00 +0200

    Adding testcase

---
+++ backend/usecases/test.js
@@ -0,0 +1,237 @@
+const axios = require('axios');
+const qs = require('qs');
+async function signIn(uname, pass) {
+    let data = qs.stringify({
+        'username': uname,
+        'password': pass
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/login',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: data
+    };
+
+    let cookie = '';
+    await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.headers['set-cookie']));
+            console.log(JSON.stringify(response.data));
+            cookie = response.headers['set-cookie'][0];
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    return cookie;
+}
+
+async function displayRequests(cookie) {
+    let data = qs.stringify({
+        'username': 'admin',
+        'password': 'admin',
+        'firstName': 'admin',
+        'lastName': 'admin',
+        'birthDate': '2000-1-1',
+        'gender': 'male',
+        'city': 'Cairo',
+        'address': 'Egypt',
+        'email': 'admin@admin',
+        'role': 'admin'
+    });
+
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/requests',
+        headers: {
+            'Cookie': cookie,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: data
+    };
+
+    await axios.request(config)
+        .then((response) => {
+            console.log("\n\n");
+            responses = response.data;
+            for (let i = 0; i < responses.length; i++) {
+                console.log(responses[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+async function acceptRequest(userId, cookie) {
+    let data = qs.stringify({
+        'action': 'accept'
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: `http://localhost:3000/requests/users/${userId}`,
+        headers: {
+            'Cookie': cookie
+        },
+        data: data
+    };
+
+    axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+}
+
+async function rejectRequest(userId, cookie) {
+    let data = qs.stringify({
+        'action': 'reject'
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: `http://localhost:3000/requests/users/${userId}`,
+        headers: {
+            'Cookie': cookie
+        },
+        data: data
+    };
+
+    await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+}
+
+async function viewMatches() {
+
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/matches', headers: {}
+    };
+
+    axios.request(config)
+        .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+                console.log(response.data[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+}
+
+async function reserveSeat(cookie, matchId, row, col) {
+    let data = {
+        "locations": [
+            {
+                "row": row,
+                "column": col
+            }
+        ],
+        "cardNumber": 20,
+        "cardPin": 60
+    };
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: `http://localhost:3000/matches/${matchId}/reservations`,
+        headers: {
+            'Cookie': cookie
+        },
+        data: data
+    };
+
+    await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+
+}
+
+async function reserveSeats(cookie, matchId, arr) {
+    let data = {
+        "locations": arr,
+        "cardNumber": 20,
+        "cardPin": 60
+    };
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: `http://localhost:3000/matches/${matchId}/reservations`,
+        headers: {
+            'Cookie': cookie
+        },
+        data: data
+    };
+
+    await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+
+}
+
+async function viewUserTickets(userId, cookie) {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `http://localhost:3000/users/${userId}/tickets`,
+        headers: {
+            'Cookie': cookie
+        }
+    };
+
+    await axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+            for (let i = 0; i < response.data.length; i++) {
+                console.log(response.data[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+}
+
+
+async function scenario() {
+    //let cookie = await signIn('admin', 'admin');
+    //await displayRequests(cookie);
+    //await acceptRequest("658de39bddb7065d122e6c7f", cookie);
+    let cookie = await signIn('moaaz2', 'moaaz');
+    //await viewMatches();
+    await reserveSeats(cookie, "656a2ff0979ed0c0bd752527", [{ "row": 2, "column": 7 }, { "row": 2, "column": 8 }]);
+    await viewUserTickets("658de39bddb7065d122e6c7f", cookie);
+}
+
+scenario();
