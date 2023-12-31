608af8d1f4f01bbd2813e83985b1ada25cb5f7b7
Moaaz Tarek <moaaztarek@hotmail.com>
2023-12-31 22:55:03 +0200

    matches view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Signup from "./component/user/Signup";
 import Login from "./component/user/Login";
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import LandingPage from "./component/user/LandingPage";
 import UserHomeTest from "./component/user/UserHomeTest";
 import Userprofile from "./component/user/Userprofile";
+import Matches from "./component/Matches/Matches.jsx";
+import Match from "./component/Matches/match.jsx";
 
 function App() {
     return (
@@ -16,6 +18,8 @@ function App() {
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/Login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
+                    <Route path="/matches" element={<Matches />} />
+                    <Route path={'/match/:id'} element={<Match/>}/>
                 </Routes>
             </BrowserRouter>
 
