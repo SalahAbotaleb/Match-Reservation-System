ae3e9b3a8d18c4fe4d28fecaab148aec0dea2504
salah <salah.m.abotaleb@gmail.com>
2024-01-11 17:10:34 +0200

    returned messages enhancement

--- backend/utils/authorizeUser.js
+++ backend/utils/authorizeUser.js
@@ -1,13 +1,13 @@
 module.exports = (userType) => {
     return function (req, res, next) {
-      if (req.session.user_id == null) {
+        if (req.session.user_id == null) {
             return res.status(401).send("Unauthorized");
         }
         if (req.session.status == "pending") {
-            return res.status(200).send("Your account is still pending");
+            return res.status(200).send("Your account is still pending.");
         }
         if (req.session.status == "rejected") {
-            return res.status(200).send("Your account request is rejected");
+            return res.status(200).send("Your account request is rejected.");
         }
         for (let i = 0; i < userType.length; i++)
             if (req.session.user_role == userType[i]) {
